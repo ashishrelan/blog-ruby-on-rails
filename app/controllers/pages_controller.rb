4ddc2e3b016a2658cceb1ba6c27fea10868669c3
Ashish Relan <ashishrelan@gmail.com>
2018-04-11 19:51:18 +0000

    alpha blog - set up home page and about page for rails application

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  
+  def initialize 
+    
+  end
+  
+  def home 
+    #puts "Welcome to the home controller "
+  end
+  
+  def about
+    
+  end  
+  
+end  
